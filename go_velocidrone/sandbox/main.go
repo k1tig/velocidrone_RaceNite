de7719774b9b339b37f2266cfaf01348b35fd95f
Max Holden <m603holden@gmail.com>
2024-12-13 14:37:39 -0500

    Started manual racer entry form

--- go_velocidrone/sandbox/main.go
+++ go_velocidrone/sandbox/main.go
@@ -1,37 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const maxWidth = 80
+
+var (
+	red    = lipgloss.AdaptiveColor{Light: "#FE5F86", Dark: "#FE5F86"}
+	indigo = lipgloss.AdaptiveColor{Light: "#5A56E0", Dark: "#7571F9"}
+	green  = lipgloss.AdaptiveColor{Light: "#02BA84", Dark: "#02BF87"}
+)
+
+type Styles struct {
+	Base,
+	HeaderText,
+	Status,
+	StatusHeader,
+	Highlight,
+	ErrorHeaderText,
+	Help lipgloss.Style
+}
+
+func NewStyles(lg *lipgloss.Renderer) *Styles {
+	s := Styles{}
+	s.Base = lg.NewStyle().
+		Padding(1, 4, 0, 1)
+	s.HeaderText = lg.NewStyle().
+		Foreground(indigo).
+		Bold(true).
+		Padding(0, 1, 0, 2)
+	s.Status = lg.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(indigo).
+		PaddingLeft(1).
+		MarginTop(1)
+	s.StatusHeader = lg.NewStyle().
+		Foreground(green).
+		Bold(true)
+	s.Highlight = lg.NewStyle().
+		Foreground(lipgloss.Color("212"))
+	s.ErrorHeaderText = s.HeaderText.
+		Foreground(red)
+	s.Help = lg.NewStyle().
+		Foreground(lipgloss.Color("240"))
+	return &s
+}
+
+//type state int
+
+const (
+// statusNormal state = iota
+// stateDone
+)
+
+type Model struct {
+	//state  state
+	lg     *lipgloss.Renderer
+	styles *Styles
+	form   *huh.Form
+	width  int
+}
+
+func NewModel() Model {
+	m := Model{width: maxWidth}
+	m.lg = lipgloss.DefaultRenderer()
+	m.styles = NewStyles(m.lg)
+
+	m.form = huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Key("class").
+				Title("FMV Name").
+				Description("Your name in the FMV Discord"),
+
+			huh.NewInput().
+				Key("time").
+				Title("Race Time:").
+				Description("Input Total Racetime for Track"),
+
+			huh.NewConfirm().
+				Key("done").
+				Title("All done?").
+				Validate(func(v bool) error {
+					if !v {
+						return fmt.Errorf("Welp, finish up then")
+					}
+					return nil
+				}).
+				Affirmative("Yep").
+				Negative("Wait, no"),
+		),
+	).
+		WithWidth(45).
+		WithShowHelp(false).
+		WithShowErrors(false)
+	return m
+}
+
+func (m Model) Init() tea.Cmd {
+	return m.form.Init()
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	var cmds []tea.Cmd
-	//var cmds []tea.Cmd
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = min(msg.Width, maxWidth) - m.styles.Base.GetHorizontalFrameSize()
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "ctrl+c":
+		case "esc", "ctrl+c", "q":
 			return m, tea.Quit
-		case "esc":
-			if m.state == tableView {
-				m.table.Blur()
-				m.state = formView
-			} else {
-				m.table.Focus()
-				m.state = tableView
-				return m, cmd
-			}
-		}
-		switch m.state {
-		case formView:
-			form, cmd := m.form.Update(msg)
-			if f, ok := form.(*huh.Form); ok {
-				m.form = f
-				m.form = f //something messed up here <~~~~
-			}
-			cmds = append(cmds, cmd)
-			return m, tea.Batch(cmds...)
-		case tableView:
-			m.table, cmd = m.table.Update(msg)
-			cmds = append(cmds, cmd)
-			return m, tea.Batch(cmds...)
 		}
 	}
+
+	var cmds []tea.Cmd
+
+	// Process the form
 	form, cmd := m.form.Update(msg)
 	if f, ok := form.(*huh.Form); ok {
 		m.form = f
@@ -43,7 +140,115 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		cmds = append(cmds, tea.Quit)
 	}
 
-	//m.table, cmd = m.table.Update(msg)
 	return m, tea.Batch(cmds...)
+}
 
+func (m Model) View() string {
+	s := m.styles
+
+	switch m.form.State {
+
+	//Racer entry confirmation
+	case huh.StateCompleted:
+
+		var b strings.Builder
+
+		fmt.Fprintf(&b, "Racer Succefully Entered: %s", m.form.GetString("class"))
+		return s.Status.Margin(0, 1).Padding(1, 2).Width(48).Render(b.String()) + "\n\n"
+	default:
+
+		var class string
+		if m.form.GetString("class") != "" {
+			class = "FMV Name: " + m.form.GetString("class")
+		}
+
+		// Form (left side)
+		v := strings.TrimSuffix(m.form.View(), "\n\n")
+		form := m.lg.NewStyle().Margin(1, 0).Render(v)
+
+		// Status (right side)
+		var status string
+		{
+			var (
+				buildInfo = "(None)"
+				role      string
+
+				level string
+			)
+
+			if m.form.GetString("time") != "" {
+				level = "Time: " + m.form.GetString("time")
+				role = m.getRole()
+				role = "\n\n" + s.StatusHeader.Render("Velocidrone User Info") + "\n" + role
+
+			}
+			if m.form.GetString("class") != "" {
+				buildInfo = fmt.Sprintf("%s\n%s", class, level)
+			}
+
+			const statusWidth = 28
+			statusMarginLeft := m.width - statusWidth - lipgloss.Width(form) - s.Status.GetMarginRight()
+			status = s.Status.
+				Height(lipgloss.Height(form)).
+				Width(statusWidth).
+				MarginLeft(statusMarginLeft).
+				Render(s.StatusHeader.Render("Current Build") + "\n" +
+					buildInfo +
+					role)
+		}
+
+		errors := m.form.Errors()
+		header := m.appBoundaryView("Charm Employment Application")
+		if len(errors) > 0 {
+			header = m.appErrorBoundaryView(m.errorView())
+		}
+		body := lipgloss.JoinHorizontal(lipgloss.Top, form, status)
+
+		footer := m.appBoundaryView(m.form.Help().ShortHelpView(m.form.KeyBinds()))
+		if len(errors) > 0 {
+			footer = m.appErrorBoundaryView("")
+		}
+
+		return s.Base.Render(header + "\n" + body + "\n\n" + footer)
+	}
+}
+
+func (m Model) errorView() string {
+	var s string
+	for _, err := range m.form.Errors() {
+		s += err.Error()
+	}
+	return s
+}
+
+func (m Model) appBoundaryView(text string) string {
+	return lipgloss.PlaceHorizontal(
+		m.width,
+		lipgloss.Left,
+		m.styles.HeaderText.Render(text),
+		lipgloss.WithWhitespaceChars("/"),
+		lipgloss.WithWhitespaceForeground(indigo),
+	)
+}
+
+func (m Model) appErrorBoundaryView(text string) string {
+	return lipgloss.PlaceHorizontal(
+		m.width,
+		lipgloss.Left,
+		m.styles.ErrorHeaderText.Render(text),
+		lipgloss.WithWhitespaceChars("/"),
+		lipgloss.WithWhitespaceForeground(red),
+	)
+}
+
+func (m Model) getRole() string {
+	return "Search for VD user data and return match here <~"
+}
+
+func main() {
+	_, err := tea.NewProgram(NewModel()).Run()
+	if err != nil {
+		fmt.Println("Oh no:", err)
+		os.Exit(1)
+	}
 }
