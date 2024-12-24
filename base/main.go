0ea9eb8cdc3caa9bc3b51cf6f182b7974c192d00
Max Holden <m603holden@gmail.com>
2024-12-24 15:42:29 -0500

    soft rebase

---
+++ base/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	rt "abc.com/base/racetools"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type vdracer struct {
+	name, qTime, craft string
+}
+type fmvracer struct {
+	name, qTime, craft string
+}
+type state uint
+
+type model struct {
+	velocidrone list.Model
+	fmv         list.Model
+	state       state
+}
+
+const (
+	vdView state = iota
+	fmvView
+)
+
+var (
+	docStyle  = lipgloss.NewStyle().Margin(1, 2)
+	itemStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("178")).
+			Background(lipgloss.Color("0")).
+			Bold(true).
+			Underline(true)
+)
+
+func (i vdracer) Title() string       { return i.name }
+func (i vdracer) Description() string { return i.qTime + " | " + i.craft }
+func (i vdracer) FilterValue() string { return i.name }
+
+func (i fmvracer) Title() string       { return i.name }
+func (i fmvracer) Description() string { return i.qTime }
+func (i fmvracer) FilterValue() string { return i.name }
+
+func (m model) Init() tea.Cmd { return nil }
+
+// Model Update
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		//filler until disable esc key
+		case "tab":
+			if m.state == fmvView {
+				m.state = vdView
+
+			} else {
+				m.state = fmvView
+				return m, cmd
+			}
+		}
+		switch m.state {
+		case vdView:
+			m.velocidrone, cmd = m.velocidrone.Update(msg)
+			cmds = append(cmds, cmd)
+			return m, tea.Batch(cmds...)
+		case fmvView:
+			m.fmv, cmd = m.fmv.Update(msg)
+			cmds = append(cmds, cmd)
+			return m, tea.Batch(cmds...)
+
+		}
+
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.velocidrone.SetSize(msg.Width-h, msg.Height-v)
+		m.fmv.SetSize(msg.Width-h, msg.Height-v)
+	}
+	return m, tea.Batch(cmds...)
+
+	//m.fmvRacers, cmd = m.fmvRacers.Update(msg)
+	//m.racers, cmd = m.racers.Update(msg)
+}
+
+func (m model) View() string {
+	m.velocidrone.DisableQuitKeybindings()
+	m.fmv.DisableQuitKeybindings()
+
+	left := docStyle.Render(m.velocidrone.View())
+	right := docStyle.Render(m.fmv.View())
+	body := lipgloss.JoinHorizontal(lipgloss.Top, left, right)
+	return body
+}
+
+func main() {
+	vdList := []list.Item{}
+	fmvList := []list.Item{}
+
+	vdRacers := rt.GetVdRacers("race.csv")
+	fmvRaw := rt.GetFMVvoice("checkin.csv")
+	fmvRacers := rt.BindLists(vdRacers, fmvRaw)
+
+	for _, v := range vdRacers {
+		vdList = append(vdList, vdracer{name: v.VelocidronName, qTime: v.QualifyingTime, craft: v.ModelName})
+	}
+
+	for _, f := range fmvRacers {
+		fmvList = append(fmvList, fmvracer{name: f.VelocidronName, qTime: f.QualifyingTime, craft: f.ModelName})
+	}
+
+	vItems := list.New(vdList, list.NewDefaultDelegate(), 0, 0)
+	vItems.Styles.Title = itemStyle //wrong style name
+
+	fmvItems := list.New(fmvList, list.NewDefaultDelegate(), 0, 0)
+	fmvItems.Styles.Title = itemStyle //wrong style name
+
+	m := model{velocidrone: vItems,
+		fmv: fmvItems,
+	}
+
+	m.velocidrone.Title = "~Velocidrone Times~"
+	m.fmv.Title = "~Checked in~"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+
+	}
+}
--- main/go.mod
+++
@@ -1,9 +0,0 @@
-module main
-
-go 1.23.4
-
-replace github.com/main/racegroup => ../main/racegroup
-
-require github.com/main/racegroup v0.0.0-00010101000000-000000000000
-
-require github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1 // indirect
