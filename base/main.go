d696cb0a342d35db3eb49ae3481d58c47309252c
K1tig <m603holden@gmail.com>
2024-12-26 23:57:42 -0500

    added table

--- base/main.go
+++ base/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 
 	rt "abc.com/base/racetools"
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -17,12 +20,14 @@ type vdracer struct {
 type fmvracer struct {
 	name, qTime, craft string
 }
+
 type state uint
 type listKeyMap struct {
 	addRacer     key.Binding
 	replaceRacer key.Binding
 	removeRacer  key.Binding
 	selectState  key.Binding
+	submitList   key.Binding
 }
 
 func newListKeyMap() *listKeyMap {
@@ -46,25 +51,41 @@ func newListKeyMap() *listKeyMap {
 			key.WithKeys("tab"),
 			key.WithHelp("tab", "switch lists"),
 		),
+
+		submitList: key.NewBinding(
+			key.WithKeys("t"),
+			key.WithHelp("t", "Submit List"),
+		),
 	}
 }
 
 const (
 	vdView state = iota
 	fmvView
+	tableView
 )
 
 type model struct {
-	keys        *listKeyMap
-	velocidrone list.Model
-	fmv         list.Model
-	state       state
+	table           table.Model
+	masterList      []*rt.Client
+	checkedInRacers []string
+	keys            *listKeyMap
+	velocidrone     list.Model
+	fmv             list.Model
+	state           state
 }
 
 var (
+	baseStyle = lipgloss.NewStyle().
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("240"))
 	docStyle  = lipgloss.NewStyle().Margin(1, 2)
 	itemStyle = lipgloss.NewStyle().
 			Foreground(lipgloss.Color("178")).
+			Bold(true).
+			Underline(true)
+	magentaStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("201")).
 			Bold(true).
 			Underline(true)
 )
@@ -163,6 +184,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case key.Matches(msg, m.keys.removeRacer):
 				index := m.fmv.Index()
 				m.fmv.RemoveItem(index)
+			case key.Matches(msg, m.keys.submitList):
+				m.checkedInRacers = m.checkIn()
+				list := m.addRacingList()
+				rows := []table.Row{}
+				for _, i := range list {
+					rows = append(rows, i)
+				}
+				m.table.SetRows(rows)
+				m.state = tableView
 			}
 		}
 		switch msg.String() {
@@ -179,7 +209,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.fmv, cmd = m.fmv.Update(msg)
 			cmds = append(cmds, cmd)
 			return m, tea.Batch(cmds...)
-
+		case tableView:
+			m.table, cmd = m.table.Update(msg)
+			cmds = append(cmds, cmd)
+			return m, tea.Batch(cmds...)
 		}
 
 	}
@@ -191,13 +224,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
+	var body string
+	switch {
 
-	m.velocidrone.DisableQuitKeybindings()
-	m.fmv.DisableQuitKeybindings()
-	left := docStyle.Render(m.velocidrone.View())
-	right := docStyle.Render(m.fmv.View())
+	case m.state != tableView:
+		m.velocidrone.DisableQuitKeybindings()
+		m.fmv.DisableQuitKeybindings()
+		left := docStyle.Render(m.velocidrone.View())
+		right := docStyle.Render(m.fmv.View())
 
-	body := lipgloss.JoinHorizontal(lipgloss.Center, left, right)
+		body = lipgloss.JoinHorizontal(lipgloss.Center, left, right)
+		return body
+	case m.state == tableView:
+		header := magentaStyle.Render("Magenta Group:")
+		body = baseStyle.Render(m.table.View())
+		body = lipgloss.JoinVertical(lipgloss.Center, header, body)
+		return body
+	}
 	return body
 }
 
@@ -233,12 +276,39 @@ func main() {
 		return []key.Binding{
 			listkeys.removeRacer,
 			listkeys.selectState,
+			listkeys.submitList,
 		}
 	}
+	columns := []table.Column{
+		{Title: "Racer", Width: 10},
+		{Title: "Time", Width: 6},
+		{Title: "Quad", Width: 10},
+	}
+	rows := []table.Row{}
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(8),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
 
 	m := model{velocidrone: vItems,
-		fmv:  fmvItems,
-		keys: listkeys,
+		fmv:        fmvItems,
+		keys:       listkeys,
+		masterList: vdRacers,
+		table:      t,
 	}
 	m.velocidrone.Title = "~Velocidrone Times~"
 	m.fmv.Title = "~FMV Preflight Checkin~"
@@ -248,4 +318,61 @@ func main() {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
+}
+
+func (m model) checkIn() []string {
+	items := m.fmv.Items()
+	var cList []string
+	for _, i := range items {
+		x := i.FilterValue()
+		cList = append(cList, x)
+	}
+	return cList
+}
+
+func (m model) addRacingList() [][]string {
+	type cleanRacer struct {
+		racer string
+		time  float64
+		quad  string
+	}
+
+	var cleanRacers []cleanRacer
+
+	var racers []*rt.Client
+
+	for _, i := range m.checkedInRacers {
+		for _, e := range m.masterList {
+			if e.VelocidronName == i {
+				racers = append(racers, e)
+				break
+			}
+		}
+	}
+
+	for _, i := range racers {
+		var x cleanRacer
+		x.racer = i.VelocidronName
+		x.quad = i.ModelName
+		var t float64
+		t, _ = strconv.ParseFloat(i.QualifyingTime, 64)
+		x.time = t
+		cleanRacers = append(cleanRacers, x)
+	}
+
+	sort.Slice(cleanRacers, func(i, j int) bool {
+		return cleanRacers[i].time < cleanRacers[j].time
+	})
+
+	var racingList [][]string
+	for _, i := range cleanRacers {
+		var racestring []string
+		racestring = append(racestring, i.racer)
+		f := i.time
+		s := strconv.FormatFloat(f, 'g', 5, 64)
+		racestring = append(racestring, s)
+		racestring = append(racestring, i.quad)
+		racingList = append(racingList, racestring)
+	}
+	return racingList
 }
