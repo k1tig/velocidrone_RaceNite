b9c94b698656ba9e75935ade16c6828c0a57aeac
K1tig <m603holden@gmail.com>
2025-01-10 00:14:53 -0500

    Soft Rebase 1

--- csvform/main.go
+++ csvform/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 
 	"strconv"
 
@@ -22,7 +23,35 @@ var (
 	green  = lipgloss.AdaptiveColor{Light: "#02BA84", Dark: "#02BF87"}
 )
 
+var (
+	baseStyle = lipgloss.NewStyle().
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("240"))
+
+	magentaStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("201")).
+			Bold(true).
+			Underline(true)
+	goldStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("220")).
+			Bold(true).
+			Underline(true)
+	cyanStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("123")).
+			Bold(true).
+			Underline(true)
+	orangStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("208")).
+			Bold(true).
+			Underline(true)
+	greenStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("34")).
+			Bold(true).
+			Underline(true)
+)
+
 var fmvTag string = `
+
 
 
 _____ __  ____     __                     
@@ -124,7 +153,34 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					m.state = fmvState
 				}
 			}
+
 			return m, cmd
+
+		case "A":
+			if m.state == fmvState {
+				x := m.fmvTable.SelectedRow()
+				m.Checkin(x)
+				fmvRows := m.makeFMVTable()
+				m.fmvTable.SetRows(fmvRows)
+			}
+
+			return m, cmd
+
+		case "G":
+			list := m.addRacingList()      // order lists of entered
+			brackets := rt.RaceArray(list) //
+			//m.makeBrackets(brackets)       //allocates racers to groups
+			indexLen := len(brackets)
+			for i := 0; i < indexLen; i++ {
+				rows := []table.Row{}
+				for _, x := range brackets[i] {
+					rows = append(rows, x)
+					m.groups[i].SetRows(rows)
+				}
+			}
+			for i := 1; i < 5; i++ {
+				m.groups[i].Blur()
+			}
 
 		}
 
@@ -180,9 +236,10 @@ func (m Model) View() string {
 	case huh.StateCompleted:
 		header := lipgloss.NewStyle().Foreground(lipgloss.Color("44"))
 		padding := lipgloss.NewStyle().Padding(0, 2)
-		accii := lipgloss.NewStyle().Padding(0, 2).Foreground(lipgloss.Color("184"))
+		accii := lipgloss.NewStyle().Padding(0, 4).Foreground(lipgloss.Color("184"))
+		tablePadding := lipgloss.NewStyle().Padding(1, 4)
 
-		vdTitle := header.Render("\n\nVelocidrone Time\n")
+		vdTitle := header.Render("\n\nVelocidrone Times\n")
 		vdTable := m.vdTable.View()
 		vdBody := padding.Render(lipgloss.JoinVertical(lipgloss.Center, vdTitle, vdTable))
 
@@ -195,9 +252,38 @@ func (m Model) View() string {
 		fmvText := accii.Render(fmvTag)
 
 		body := lipgloss.JoinHorizontal(lipgloss.Center, tables, fmvText)
-		footer := "\n\nUse 'tab' to change lists"
+		footer := "\n\nUse 'tab' to change lists\n\n"
 		view := lipgloss.JoinVertical(lipgloss.Left, body, footer)
-		return view
+
+		//bracket groups section
+		goldHeader := goldStyle.Render("Gold Group:")
+		goldBody := baseStyle.Render(m.groups[0].View())
+		gold := lipgloss.JoinVertical(lipgloss.Center, goldHeader, goldBody)
+
+		mHeader := magentaStyle.Render("Magenta Group:")
+		mBody := baseStyle.Render(m.groups[1].View())
+		magenta := lipgloss.JoinVertical(lipgloss.Center, mHeader, mBody)
+
+		cyanHeader := cyanStyle.Render("Cyan Group:")
+		cyanBody := baseStyle.Render(m.groups[2].View())
+		cyan := lipgloss.JoinVertical(lipgloss.Center, cyanHeader, cyanBody)
+
+		orangeHeader := orangStyle.Render("Orange Group:")
+		orangeBody := baseStyle.Render(m.groups[3].View())
+		orange := lipgloss.JoinVertical(lipgloss.Center, orangeHeader, orangeBody)
+
+		greenHeader := greenStyle.Render("Green Group:")
+		greenBody := baseStyle.Render(m.groups[4].View())
+		green := lipgloss.JoinVertical(lipgloss.Center, greenHeader, greenBody)
+
+		r1s := lipgloss.JoinHorizontal(lipgloss.Center, gold, magenta, cyan, orange, green)
+		r1 := tablePadding.Render(r1s)
+		//r2 := lipgloss.JoinHorizontal(lipgloss.Center, orange, green)
+
+		//groupBody := lipgloss.JoinVertical(lipgloss.Center, r1, r2)
+
+		layout := lipgloss.JoinVertical(lipgloss.Center, view, r1)
+		return layout
 
 	default:
 
@@ -246,8 +332,9 @@ func NewModel() Model {
 		WithShowErrors(false)
 
 	gColumns := []table.Column{
-		{Title: "Name", Width: 16},
-		{Title: "Time", Width: 8},
+		{Title: "Name", Width: 11},
+		{Title: "Time", Width: 6},
+		{Title: "Craft", Width: 9},
 	}
 	vdColumns := []table.Column{
 		{Title: "Name", Width: 16},
@@ -263,11 +350,11 @@ func NewModel() Model {
 
 	rows := []table.Row{}
 
-	groupTable := table.New( //for color groups display
+	gt := table.New( //for color groups display
 		table.WithColumns(gColumns),
 		table.WithRows(rows),
 		table.WithFocused(false),
-		table.WithHeight(6),
+		table.WithHeight(8),
 	)
 
 	vdTable := table.New(
@@ -296,9 +383,9 @@ func NewModel() Model {
 
 	var groupTlist []table.Model
 
-	for i := 0; i < 5; i++ {
-		groupTlist = append(groupTlist, groupTable)
-	}
+	g1, g2, g3, g4, g5 := gt, gt, gt, gt, gt
+	groupTlist = append(groupTlist, g1, g2, g3, g4, g5) // fix this ugly thing, just broken down to test
+
 	fmvTable.SetStyles(s)
 	vdTable.SetStyles(s)
 
@@ -321,6 +408,8 @@ func (m Model) makeFMVTable() []table.Row {
 		qtime := i.QualifyingTime
 		if i.Status == fmvNul.Status {
 			status = "Missing"
+		} else {
+			status = i.Status
 		}
 		s = append(s, name, qtime, status)
 		rows = append(rows, s)
@@ -340,3 +429,70 @@ func (m Model) makeVDTable() []table.Row {
 	}
 	return rows
 }
+
+func (m Model) Checkin(r table.Row) {
+	for _, i := range m.fmvVoiceList {
+		if r[0] == i.VdName {
+			i.Status = "Entered"
+		}
+	}
+}
+
+func (m Model) addRacingList() [][]string {
+	type cleanRacer struct {
+		racer string
+		time  float64
+		quad  string
+	}
+
+	var cleanRacers []cleanRacer
+	var racers []*rt.FmvVoicePilot
+
+	for _, i := range m.fmvVoiceList {
+		if i.Status == "Entered" {
+			racers = append(racers, i)
+		}
+	}
+
+	for _, i := range racers {
+		var x cleanRacer
+		x.racer = i.VdName
+		x.quad = i.ModelName
+		var t float64
+		t, _ = strconv.ParseFloat(i.QualifyingTime, 64)
+		x.time = t
+		cleanRacers = append(cleanRacers, x)
+	}
+
+	sort.Slice(cleanRacers, func(i, j int) bool {
+		return cleanRacers[i].time < cleanRacers[j].time
+	})
+
+	var racingList [][]string
+	for _, i := range cleanRacers {
+		var racestring []string
+		racestring = append(racestring, i.racer)
+		f := i.time
+		s := strconv.FormatFloat(f, 'g', 5, 64)
+		racestring = append(racestring, s)
+		racestring = append(racestring, i.quad)
+		racingList = append(racingList, racestring)
+	}
+	return racingList
+}
+
+/*
+func (m Model) makeBrackets(brackets [][][]string) {
+	indexLen := len(brackets)
+	for i := 0; i < indexLen; i++ {
+		rows := []table.Row{}
+		for _, x := range brackets[i] {
+			rows = append(rows, x)
+			m.groups[i].SetRows(rows)
+		}
+	}
+	for i := 1; i < 5; i++ {
+		m.groups[i].Blur()
+	}
+}
+*/
