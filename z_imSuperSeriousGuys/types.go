564489332b9b4b9f7e741b794473bdf916f3ea7e
Max Holden <m603holden@gmail.com>
2025-03-07 20:44:57 -0500

    power loss

--- z_imSuperSeriousGuys/types.go
+++ z_imSuperSeriousGuys/types.go
@@ -1,8 +1,15 @@
 package main
 
+//Pilot stuff
 type Pilot struct {
 	DiscordName    string `csv:"Display Name" json:"displayname"` // discord
 	VdName         string `csv:"Player Name" json:"vdname"`
 	QualifyingTime string `csv:"Lap Time" json:"qualifytime"`
 	ModelName      string `csv:"Model Name" json:"modelname"`
+	Id             string `csv:"ID" json:"id"`
+	Status         string //used for checkin placeholder
 }
+
+var NulPilot = Pilot{Status: "-"}
+
+type Styles struct{}
