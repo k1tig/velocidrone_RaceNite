3d1cd2e6306880c1cb3609a268301187ba838599
K1tig <m603holden@gmail.com>
2025-03-18 08:57:50 -0400

    fixed bug in discord to fmv sheet. don't use shared blank phrases

--- z_imSuperSeriousGuys/groupBuilder.go
+++ z_imSuperSeriousGuys/groupBuilder.go
@@ -59,51 +59,6 @@ func GetDiscordId(discordCsv string) []Pilot {
 	return DiscordRecords
 }
 
-func RaceArray(vdList [][]string) [][][]string {
-	//[group of colors] [group of racers] [racer list of details]
-	//makes a group of groups with the total amount of racers not exceeding a +1 differential
-	var maxGroupsize = 8
-	var grouplength int
-	var totalGroups int
-	var modulus int
-
-	racers := len(vdList)
-	if racers > 40 {
-		maxGroupsize = 10
-	}
-
-	for i := 1; i <= maxGroupsize; i++ {
-		if racers/i <= maxGroupsize {
-			totalGroups = i
-			modulus = racers % i
-			if modulus == 0 {
-				grouplength = racers / i
-			} else {
-				grouplength = (racers - modulus) / i
-			}
-			break
-		}
-	}
-
-	var groupStructure = make([][][]string, totalGroups)
-	var c int
-	x := modulus
-
-	for i := 1; i <= totalGroups; i++ {
-		if x > 0 { // distribues the modulus between the lower teir groups
-			racers := vdList[c : i*(grouplength+1)]
-			groupStructure[i-1] = racers
-			x--
-			c += grouplength + 1
-		} else { // groups that don't take a modulus
-			racers := vdList[c : c+grouplength]
-			groupStructure[i-1] = racers
-			c += grouplength
-		}
-	}
-	return groupStructure
-}
-
 func BindLists(velocidroneList, fmvList, discordCheatSheet []Pilot) []Pilot {
 	var NulPilot Pilot
 	//discordCheatSheet is the DIY list a group can keep of discord usr IDs and VD names in CSV
